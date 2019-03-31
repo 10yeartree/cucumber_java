3ceeaa1e65182a75d137265bbc7a1e8d561dd6e9
bfx <abc@hahaahah.com>
2019-03-31 19:37:47 +0800

    test

--- steps/wabi/web/Web.java
+++ steps/wabi/web/Web.java
@@ -3,22 +3,34 @@ package wabi.web;
 import cucumber.api.java.en.Given;
 import tree.Hook;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
 
 public class Web {
-	public static ChromeDriver driver = null;
-	
+	public static WebDriver driver = null;
+
 	@Given("^Open the browser$")
 	public void i_open_browser() throws Exception {
-		driver = new ChromeDriver();
+		String driverType = System.getenv("BROWSER");
+		if (driverType == null) {
+			driverType = "Chrome";
+		}
+
+		if (driverType.equals("Chrome")) {
+			driver = new ChromeDriver();
+		} else if (driverType.equals("Firefox")) {
+			driver = new FirefoxDriver();
+		} else {
+			throw new Exception("Not supported browser type: " + driverType);
+		}
 		driver.manage().window().maximize();
 	}
-	
+
 	@Given("^Close the browser$")
 	public void i_close_browser() throws Exception {
 		driver.close();
 	}
-	
+
 	@Given("^Get url (.*?)$")
 	public void i_open_browser(String url) throws Exception {
 		url = Hook.bigmap.containsKey(url) ? Hook.bigmap.get(url) : url;
